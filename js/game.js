aa15a80757425a1dee27f7da11c8ad53df9a38d3
luckymucky091-coder <luckymucky091@gmail.com>
2026-07-24 08:39:58 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,59 +1,6 @@
-export class Game{
-
-    constructor(){
-
-        this.canvas=document.getElementById("gameCanvas");
-
-        this.ctx=this.canvas.getContext("2d");
-
-        this.resize();
-
-        window.addEventListener("resize",()=>this.resize());
-
-    }
-
-    resize(){
-
-        this.canvas.width=1280;
-        this.canvas.height=720;
-
-    }
-
-    start(){
-
-        requestAnimationFrame(()=>this.loop());
-
-    }
-
-    loop(){
-
-        this.update();
-
-        this.render();
-
-        requestAnimationFrame(()=>this.loop());
-
-    }
-
-    update(){
-
-    }
-
-    render(){
-
-        this.ctx.fillStyle="#6EC6FF";
-        this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
-
-        this.ctx.fillStyle="white";
-        this.ctx.font="40px Arial";
-
-        this.ctx.fillText("Quiz Land P6",40,60);
-
-    }
-
-}
 import { SceneManager } from "./scene.js";
 import { Camera } from "./camera.js";
+import { Input } from "./input.js";
 
 import { BootScene } from "./scenes/BootScene.js";
 import { MenuScene } from "./scenes/MenuScene.js";
@@ -69,12 +16,17 @@ export class Game {
         this.canvas.width = 1280;
         this.canvas.height = 720;
 
+        window.addEventListener("resize", () => this.resize());
+        this.resize();
+
         this.lastTime = 0;
         this.deltaTime = 0;
 
         this.fps = 0;
         this.frames = 0;
         this.timer = 0;
+
+        this.input = new Input();
 
         this.camera = new Camera();
 
@@ -83,6 +35,12 @@ export class Game {
         this.sceneManager.add("boot", new BootScene(this));
         this.sceneManager.add("menu", new MenuScene(this));
         this.sceneManager.add("play", new PlayScene(this));
+    }
+
+    resize() {
+
+        this.canvas.width = 1280;
+        this.canvas.height = 720;
 
     }
 
@@ -90,27 +48,25 @@ export class Game {
 
         this.sceneManager.change("boot");
 
-        requestAnimationFrame((t)=>this.loop(t));
+        requestAnimationFrame((time) => this.loop(time));
 
     }
 
-    loop(time){
+    loop(time) {
 
-        this.deltaTime=(time-this.lastTime)/1000;
+        this.deltaTime = (time - this.lastTime) / 1000;
+        this.lastTime = time;
 
-        this.lastTime=time;
-
-        this.timer+=this.deltaTime;
-
+        this.timer += this.deltaTime;
         this.frames++;
 
-        if(this.timer>=1){
+        if (this.timer >= 1) {
 
-            this.fps=this.frames;
+            this.fps = this.frames;
 
-            this.frames=0;
+            this.frames = 0;
 
-            this.timer=0;
+            this.timer = 0;
 
         }
 
@@ -118,10 +74,8 @@ export class Game {
 
         this.sceneManager.render(this.ctx);
 
-        requestAnimationFrame((t)=>this.loop(t));
+        requestAnimationFrame((time) => this.loop(time));
 
     }
 
 }
-import { Input } from "./input.js";
-this.input = new Input();
